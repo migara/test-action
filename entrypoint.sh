29d4d4d6d76415930415b37cc9e60fa760a0a3c1
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-03-12 23:53:03 +0000

    test action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,19 @@ printf "\n\n"
 echo "# Open Policy Agent"
 /opa version
 printf "\n\n"
-ls ${GITHUB_WORKSPACE}
 IFS=';'
 mapfile -t lines < <(echo "$INPUT_TESTS" | grep -v "^$")
 
 e_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
-  # cmd="/opa eval ${args[@]} $INPUT_OPTIONS"
-  # cmd="/opa eval -i $INPUT_POLICY -d $INPUT_TESTS "data.panos[x]" --format pretty"
-  cmd="/opa eval -i ${GITHUB_WORKSPACE}/policy.yml -d  ${GITHUB_WORKSPACE}/tests/panos.rego "data.panos[x]" --format pretty"
+  cmd="/opa eval -i ${GITHUB_WORKSPACE}/$INPUT_POLICY -d  ${GITHUB_WORKSPACE}/$INPUT_TESTS "data.panos" --format json"
   echo " 🚀 Running: $cmd"
   printf "\n"
-  # echo " 🚀 Running: $1 $2 $INPUT_POLICY"
-  # printf "\n"
-  eval "$cmd" || e_code=1
+  # eval "$cmd" || e_code=1
+  opa_results=$(cmd)
+  echo $opa_results
+  echo "::set-output name=opa_results::$opa_results"
   printf "\n\n"
 done
 

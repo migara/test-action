095c047125ee63a20da0c9c5f841f352fd95a094
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-03-13 00:19:44 +0000

    test action

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,14 +11,15 @@ e_code=0
 for line in "${lines[@]}"; do
   read -r -a args <<< "$line"
   cmd=`/opa eval -i ${GITHUB_WORKSPACE}/$INPUT_POLICY -d  ${GITHUB_WORKSPACE}/$INPUT_TESTS "data.panos" --format json`
-  echo " 🚀 Running: $cmd"
-  printf "\n"
+  # echo " 🚀 Running: $cmd"
+  # printf "\n"
   # eval "$cmd" || e_code=1
-  opa_results=$(cmd)
-  echo $opa_results
+  # opa_results=$(cmd)
+  # echo $opa_results
   # echo "::set-output name=opa_results::$opa_results"
-  echo "::set-output name=opa_results::$(echo $cmd)"
-  printf "\n\n"
+  # echo "::set-output name=opa_results::$(echo $cmd)"
+  echo "::set-output name=opa_results::foo"
+  # printf "\n\n"
 done
 
 exit $e_code
